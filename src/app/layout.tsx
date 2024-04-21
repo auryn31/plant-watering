0831c4072e184b56808799194bd03cc501ac0277
Auryn Engel <auryn_engel@mckinsey.com>
2024-04-21 22:53:58 +0200

    update to plantw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
             <div className="navbar bg-base-100">
               <div className="flex-1">
                 <a className="btn btn-ghost text-xl" href="/">
-                  PlantR
+                  PlantW
                 </a>
               </div>
               <div className="flex-none">
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,9 @@
 export default function Login() {
   return (
     <main className="flex flex-col gap-4 justify-center w-full h-screen grow items-center">
-      <h1 className="text-4xl">PlantR</h1>
+      <h1 className="text-4xl">PlantW</h1>
       <a className="btn btn-accent" href="/api/auth/login">
-        Login
+        Login/Singup
       </a>
     </main>
   );
