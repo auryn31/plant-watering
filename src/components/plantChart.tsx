f56a1e4f362b303723a714e414fe6e21ff066e24
Auryn Engel <auryn_engel@mckinsey.com>
2024-05-03 09:29:38 +0200

    add watering to chart

--- src/components/plantChart.tsx
+++ src/components/plantChart.tsx
@@ -3,6 +3,9 @@ import { PlantWithValues } from "@/sql/types";
 import Chart from "react-apexcharts";
 import { Props } from "react-apexcharts";
 
+const GREEN = "#15803d";
+const BLUE = "#1d4ed8";
+
 const PlantChart: React.FC<Pick<PlantWithValues, "values">> = ({ values }) => {
   const displayValues = values
     .filter(
@@ -11,12 +14,18 @@ const PlantChart: React.FC<Pick<PlantWithValues, "values">> = ({ values }) => {
     .slice(0, 60)
     .reverse();
   const humidityData = displayValues.map((it) => it.humidity);
+  const watering = displayValues.map((it) => it.last_watering_in_ml);
   const createdData = displayValues.map((it) => it.created_at?.getTime());
 
   const series = [
     {
       name: "Humidity in %",
       data: humidityData,
+    },
+    {
+      name: "Last watering in ml",
+      data: watering,
+      type: "bar",
     },
   ];
   const chartConfig: Props = {
@@ -38,7 +47,7 @@ const PlantChart: React.FC<Pick<PlantWithValues, "values">> = ({ values }) => {
       dataLabels: {
         enabled: false,
       },
-      colors: ["#fff"],
+      colors: [GREEN, BLUE],
       stroke: {
         lineCap: "round",
         curve: "smooth",
@@ -63,17 +72,42 @@ const PlantChart: React.FC<Pick<PlantWithValues, "values">> = ({ values }) => {
         categories: createdData,
         type: "datetime",
       },
-      yaxis: {
-        min: 0,
-        max: 100,
-        labels: {
-          style: {
-            colors: "#616161",
-            fontSize: "12px",
-            fontWeight: 400,
+      yaxis: [
+        {
+          min: 0,
+          max: 100,
+          title: {
+            text: "Humidity in %",
+            style: {
+              color: GREEN,
+            },
+          },
+          labels: {
+            style: {
+              colors: GREEN,
+              fontSize: "12px",
+              fontWeight: 400,
+            },
           },
         },
-      },
+
+        {
+          opposite: true,
+          title: {
+            text: "Watering in ml",
+            style: {
+              color: BLUE,
+            },
+          },
+          labels: {
+            style: {
+              colors: BLUE,
+              fontSize: "12px",
+              fontWeight: 400,
+            },
+          },
+        },
+      ],
       fill: {
         opacity: 0.8,
       },
